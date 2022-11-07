fa971a9ef82f6cf543b02415a51d97552bde4117
SumRandomPerson <mikegui343@gmail.com>
2022-11-07 08:59:24 -0500

    changes

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,11 +7,16 @@ public class RubyController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal; 
     float vertical;
+
+    public int maxHealth = 5;
+    int currentHealth;
     
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+
     }
 
     // Update is called once per frame
@@ -28,5 +33,10 @@ public class RubyController : MonoBehaviour
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+    
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
     }
 }
