18c09662045bee1728fe4e5561b06b1800ea9379
SumRandomPerson <mikegui343@gmail.com>
2022-12-14 13:24:37 -0500

    changes

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -10,7 +10,7 @@ public class RubyController : MonoBehaviour
     
     public int maxHealth = 5;
     public float timeInvincible = 2.0f;
-    
+
     public int health { get { return currentHealth; }}
     int currentHealth;
     
@@ -21,10 +21,15 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
     
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+    
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        
         currentHealth = maxHealth;
     }
 
@@ -33,6 +38,18 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        
+        Vector2 move = new Vector2(horizontal, vertical);
+        
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
         
         if (isInvincible)
         {
@@ -55,6 +72,7 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
             
@@ -66,5 +84,4 @@ public class RubyController : MonoBehaviour
         Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
-
 
